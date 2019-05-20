af078cf1e9d145172c1401b6432d6015d0510bbc
Frederik Declercq <frederikdeclercq@hotmail.com>
2019-05-20 16:18:21 +0200

    finished the exercise with the help of madalina

--- 1.Global-Supervariables/login.php
+++ 1.Global-Supervariables/login.php
@@ -7,19 +7,14 @@
     <title>Login</title>
 </head>
 <body>
-<form action="trylog.php" method = "post">
-        <label for="username">Username</label> <input type="username" id="usename" name="username"><br /><br />
-        <label for="password">Password:</label> <input type="text" id="password" name="password"><br /><br />
+<form action="trylog.php" method ="post">
+        <label for="username">Username</label> <input type="username" id="username" name="username"><br/><br/>
+        <label for="password">Password:</label> <input type="text" id="password" name="password"><br/><br/>
         <button type="submit">Login</button>
     </form>
 
-<?php
-if (!empty($_POST)) {
-    echo 
 
-
-
-    /?>
-    
-</body>
+    </body>
 </html>
+
+
---
+++ 1.Global-Supervariables/trylog.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login result message</title>
+</head>
+<body>
+
+<?php
+if (!empty ($_POST['username']) && !empty ($_POST['password'])){
+
+    $username = 'Fred';
+    $password = 'becode'; 
+
+if ($_POST['username'] == $username && $_POST['password'] == $password){
+    echo "Congrats, you are in!";
+}
+else {
+    echo
+    "Wrong username or password";
+}
+}
+
+
+
+else {
+    echo
+    "Please fill out something in the fields";
+}
+
+?>
+
+</body>
+</html>
