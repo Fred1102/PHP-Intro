9a8e798198c88f4993fa893ec0ff86ef3bd697d1
Frederik Declercq <frederikdeclercq@hotmail.com>
2019-05-20 10:31:32 +0200

    last commit

--- 1.Global-Supervariables/result.php
+++ 1.Global-Supervariables/result.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel = "stylesheet" href = "stylesheet.css">
     <title>Document</title>
 </head>
 
@@ -13,12 +15,14 @@
 <?php
 if (!empty($_POST)) {
     echo 
-    "<ul><li>{$_POST["movie1"]} </li> 
+"<p>My favourite movie is</p>
+<ul><li>{$_POST["movie1"]} </li> 
 	<li>{$_POST["movie2"]}</li>
 	<li>{$_POST["movie3"]}</li>
 	<li>{$_POST["movie4"]}</li>
     <li>{$_POST["movie5"]}</li></ul>".
-"<ul><li>{$_POST["tvshow1"]} </li> 
+"<p>My favourite tv show is</p>
+<ul><li>{$_POST["tvshow1"]} </li> 
 	<li>{$_POST["tvshow2"]}</li>
 	<li>{$_POST["tvshow3"]}</li>
 	<li>{$_POST["tvshow4"]}</li>
@@ -27,7 +31,7 @@ if (!empty($_POST)) {
 
 else { echo
 "<p>My favourite country is</p><ul><li>{$_GET["favouritecountry"]}</li></ul>".
-"<ul><li>{$_GET["worstmovie"]}</li></ul>";
+"<p>my worst movie is </p><ul><li>{$_GET["worstmovie"]}</li></ul>";
 }
     ?>
 
--- 1.Global-Supervariables/setup.php
+++ 1.Global-Supervariables/setup.php
@@ -9,17 +9,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel = "stylesheet" href = "stylesheet.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
     
 <form action="result.php" method="POST">
+<p>My favourity movie is </p>
+
     <input type="text" name="movie1"> <br>
     <input type="text" name="movie2"> <br>
     <input type="text" name="movie3"> <br>
     <input type="text" name="movie4"> <br>
     <input type="text" name="movie5"> <br>
+
+ <p>My favourity tv show is </p>
+
 
     <input type="text" name="tvshow1"> <br>
     <input type="text" name="tvshow2"> <br>
@@ -33,6 +39,7 @@
 <p>My favourity country is </p>
 <form action="result.php" method="GET">
     <input type="text" name="favouritecountry"> <br>
+    <p>My worst movie is </p>
     <input type="text" name="worstmovie"> <br>
 
     <button type="submit"> SUBMIT</button>
