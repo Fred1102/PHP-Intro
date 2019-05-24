efaf16dc5fe492114cfa7c00b0081336673ddbfb
Frederik Declercq <frederikdeclercq@hotmail.com>
2019-05-24 10:35:41 +0200

    centered the echo messages

--- 2.Loops/Lottery/input.php
+++ 2.Loops/Lottery/input.php
@@ -6,20 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Lottery</title>
 </head>
 <body>
 
 <form action="lottery.php" method="post" class="w-25 p-3 mx-auto text-center">
 <div>
-<img src="🍕" alt="pizza"><input type="radio" name="option" value="pizza">
+<img src="img/pizza.png" alt="pizza"><input type="radio" name="option" value="pizza">
 </div>
 <div>
-<img src="🍟" alt="fries"><input type="radio" name="option" value="fries">
+<img src="img/hamburger.png" alt="hamburger"><input type="radio" name="option" value="hamburger">
 </div>
 <div>
-<img src="🍔" alt="burger"><input type="radio" name="option" value="burger">
+<img src="img/fries.png" alt="fries"><input type="radio" name="option" value="fries">
 </div>
+<button type="submit">Try your good luck!</button>
 </form>
 
 
--- 2.Loops/Lottery/lottery.php
+++ 2.Loops/Lottery/lottery.php
@@ -1,5 +1,46 @@
 <?php
 
 
+if(!empty ($_POST['option'])){
+    $input =array("hamburger","pizza","fries");
+    $randomnumber = rand(0,2);
+    $userchoice = $_POST['option'];
+    $computerchoice = $input[$randomnumber];  
+} 
+
+if ($userchoice == $computerchoice) {
+    echo "<p class=\"w-25 p-3 mx-auto text-center\">  You won! Want to play again? </p>";
+}
+else {
+    echo "<p class=\"w-25 p-3 mx-auto text-center\"> You lost! </p>";
+}
+
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Lottery</title>
+</head>
+<body>
+
+<form action="input.php" metaphone="post"class="w-25 p-3 mx-auto text-center">
+<div>
+<img src="img/<?php echo $userchoice?>.png" alt="hamburger"></div> 
+<div>
+<img src="img/<?php echo $computerchoice?>.png" alt="fries"></div> 
+</form>
+
+
+<form action="input.php" method="post" class="w-25 p-3 mx-auto text-center">
+    <button type="submit">Try again!</button>
+</form>
+    
+</body>
+</html>
