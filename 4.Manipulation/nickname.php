c152a09a96c09c9ec16dd55a8f56816b8fcb355b
Frederik Declercq <frederikdeclercq@hotmail.com>
2019-05-28 16:12:44 +0200

    alphabet + digits

--- 4.Manipulation/nickname.php
+++ 4.Manipulation/nickname.php
@@ -2,8 +2,26 @@
 
 
 if(isset($_POST["user"])){
-    echo strrev(strtoupper(strrev($_POST["user"])));
-}   
+
+
+$alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+$alphabet_capitalised = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z'];
+$digits = [0,1,2,3,4,5,6,7,8,9];
+
+$nigits = array_merge($alphabet, $alphabet_capitalised, $digits);
+
+$indexs = array_rand($nigits,4);
+var_dump($indexs);
+$simbols = array();
+for ($i=0; $i<count($indexs); ++$i){
+    array_push($simbols, $nigits[$indexs[$i]]);
+}
+$final_string = implode($simbols);
+
+echo $final_string."X". "- " .strrev(strtoupper(strrev($_POST["user"])))." -";
+
+
+}
 
 ?>
 
